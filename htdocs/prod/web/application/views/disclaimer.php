882bb4fb1b509daae75a2887d3f57b0d88bc7e49
Mike Stinson <mike.stinson@stjoseph.com>
2015-10-22 09:23:41 -0400

    no message

---
+++ htdocs/prod/web/application/views/disclaimer.php
@@ -0,0 +1,27 @@
+</header>
+<main class='disclaimer'>
+    <h1>Disclaimer</h1>
+    <p>&nbsp;</p>
+    <p>
+        This Website is owned and provided by Highland Farms, and is prepared solely for your interest, information and education. Your access to and continued use of the Website constitutes your acceptance of and compliance to the following terms of this Policy, which may change at any time without notice. If you are uncomfortable or disagree with any of the terms of this Policy as outlined below, please discontinue any use of our Website.
+    </p>
+
+    <p>
+        On this website, Highland Farms or the Company are used to refer collectively to Highland Farms and all of its subsidiaries, except where the context requires otherwise.
+    </p>
+    <p> 
+        Your use of our Website is limited to personal and non-commercial use only, unless other use by you is specifically permitted by Highland Farms in writing. You may make a single copy of or download information supplied on the Website, provided the World Wide Web Address (http://www.freshathighlandfarms.com) is visible on the material. The content shown on the Website is protected by copyright, trademark and other laws and you are not permitted to copy, modify, reuse, transmit, distribute or re-post any of the content displayed (except as stated by in this Policy) without written permission from Highland Farms.
+    </p>
+    <p>
+        The information on this Website is believed to be accurate and complete at the time of posting. Highland Farms attempts to ensure all information remains accurate and current, but does not guarantee the accuracy, completeness and quality at all times. Changes to the content of the Website are made periodically, without notice.
+    </p>
+    <p>  
+        This Website may contain forward-looking statements about future performance of Highland Farms. These statements are based on management's assumptions and beliefs in light of the information currently available to them. Highland Farms assumes no obligation to update such forward-looking statements on this Website, and these statements are subject to uncertainties and other factors that could cause actual results to materially differ. Your use of and browsing within the Website is at your own risk. Highland Farms is not liable for any direct, indirect, incidental, or punitive damage resulting from your access to or use of this Website. Highland Farms is also not responsible for any damages to your computer equipment or other property on account of your access to and downloading from the Website. You must therefore take precautions to ensure any information you access or download is free of viruses, worms, trojan horses and other items of a destructive nature. Access to this Website cannot be guaranteed at any time.
+    </p>  
+    <p>
+        This Website contains "links" to other Websites, which we think are of convenience and interest. However, we do not control these third party Websites and are not responsible for the content of any off-site pages. Linking to such sites is at your own risk, as Highland Farms does not verify or investigate the information contained within them.
+    </p>
+    <p>
+        On this Website, "Highland Farms" or the "Company" are used to refer collectively to Highland Farms and all of its subsidiaries, except where the context requires otherwise. However, we do not control these third party Websites and are not responsible for the content of any off-site pages. Linking to such sites is at your own risk, as Highland Farms does not verify or investigate the information contained within them.
+    </p>    
+</main>
