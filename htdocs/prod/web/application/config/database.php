c10d784939cfa014e2dee6223581657302829788
Sean Macdonald <sean@crazyhorsecoding.com>
2015-10-26 05:49:15 -0400

    switched database credentials from stinson to AWS

--- htdocs/prod/web/application/config/database.php
+++ htdocs/prod/web/application/config/database.php
@@ -73,6 +73,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+
+//	RDS
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'valhalla-prod.cb1qb4plxjpf.us-east-1.rds.amazonaws.com',
+	'username' => 'highland',
+	'password' => '1mn9alXLS65zYbK3pHxRgjC',
+	'database' => 'webdev_hlf',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+/* stinson server
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => '167.88.112.253',
@@ -94,3 +119,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+*/
