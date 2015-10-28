d5f4f64151330627a46de1c68e62e5dc0b78682c
Anirudh Swaminathan <aswaminathan@P15013.local>
2015-10-28 13:17:08 -0400

    updated Inside the Store copy

--- htdocs/prod/web/application/views/inside-store.php
+++ htdocs/prod/web/application/views/inside-store.php
@@ -2,104 +2,47 @@
 // :|
 $inside = array();
 
-$inside[] = array(
-    "title" => "Fresh Bakery",
-    "subtitle" => "Find Your Slice of Heaven",
-    "description" => "There's nothing more inviting than the delicious aroma of freshly baked bread. At Highland Farms, you'll find a vast selection of breads, everything from French baguettes to Mediterranean pitas to North American bagels. Satisfy your sweet tooth with Highland Farms' mouth-watering variety of freshly baked goodies including cakes, cookies, pies, tarts and so much more. Having a special celebration? Stop by our bakery with your special order and let our pastry chef create an unforgettable delicacy to make it extra special. Just follow your nose to the bakery at Highland Farms for more details!",
-    "image" => "assets/images/store_bakery_web_crop.jpg",
-    "class" => "brown"
-);
+$inside[] = array(     "title" => "Fresh Bakery",     "subtitle" => "Find Your
+Slice of Heaven",     "description" => "Follow your nose to the bakery where
+you'll find freshly baked breads and all sorts of sweet temptations. Having a
+celebration? Stop by our bakery with your special order.",     "image" =>
+"assets/images/store_bakery_web_crop.jpg",     "class" => "brown" );
 
-$inside[] = array(
-    "title" => "Dairy",
-    "subtitle" => "Freshest Dairy Goodness",
-    "description" => "Farm fresh dairy products like milk, butter, creams and soft cheeses always bring home that traditional country feeling. Highland Farms continues its commitment to freshness by offering you a wide variety of dairy products, including yogurt, milk, cheeses and spreads, as well as an extensive line-up of non-dairy and organic alternatives. Highland Farms offers you a fresh way to start your day every day with farm-fresh, great-tasting and wholesome dairy products.",
-    "image" => "assets/images/store_dairy_web_crop.jpg",
-    "class" => "blue"
-);
+$inside[] = array(     "title" => "Dairy",     "subtitle" => "Fresh Dairy
+Goodness",     "description" => "We have a wide selection of farm fresh dairy products including yogurt, milk, cheeses and spreads, as well as an extensive line-up of non-dairy and organic alternatives.",     "image" =>
+"assets/images/store_dairy_web_crop.jpg",     "class" => "blue" );
 
-$inside[] = array(
-    "title" => "Fresh Deli",
-    "subtitle" => "Always a Cut Above",
-    "description" => "Visit the fresh deli counter at Highland Farms and get a taste of pure old-market freshness. Our deli meats and cheeses are cut-to-order, meaning that you always get exactly what you need. Whether it's ham, salami, prosciutto, provolone or havarti, you know you're getting the freshest selection at the Highland Farms deli counter. <br><br>Highland Farms also has a mouth-watering selection of olives, antipastos and fresh deli salads. From sundried tomatoes, to our deluxe mixed bean salad, to Jumbo Calamata olives, it's all served fresh for you, everyday!",
-    "image" => "assets/images/store_deli_web_crop.jpg",
-    "class" => "orange"
-);
+$inside[] = array(     "title" => "Fresh Deli",     "subtitle" => "Always a
+Cut Above",     "description" => "Visit our deli counter for a traditional market experience. Deli meats and cheeses are cut-to-order by our friendly staff. You'll also find a mouth-watering selection of olives, antipastos and fresh deli salads.",     "image" =>
+"assets/images/store_deli_web_crop.jpg",     "class" => "orange" );
 
-$inside[] = array(
-    "title" => "Seafood",
-    "subtitle" => "An Ocean of Freshness",
-    "description" => "If you love seafood, Highland Farms is the ideal place to shop for a fresh selection of fish like herring, salmon, squid, lobster and more. Our extensive variety will give you a virtually limitless selection of creative ideas to make your seafood meals extra special. With a daily selection of fresh fish, shopping at Highland Farms is a must for seafood lovers.",
-    "image" => "assets/images/store_seafood_web_crop.jpg",
-    "class" => "teal"
-);
+$inside[] = array(     "title" => "Seafood",     "subtitle" => "Fish for Flavour",     "description" => "Our fish counter is a seafood lover's dream. Every day, you'll find a fresh selection of fish. If you have any questions, don't hesitate to ask one of our knowledgeable fishmongers.",     "image" => "assets/images/store_seafood_web_crop.jpg",
+"class" => "teal" );
 
-$inside[] = array(
-    "title" => "Flowers",
-    "subtitle" => "Say it with Flowers",
-    "description" => "Whether you're looking for an exotic bouquet for that special someone, or a beautiful potted plant for home, you won't find a better selection than Highland Farms Flower Shop. Our specially trained assistants will help you choose from a variety of fresh blossoms, or create a customized bouquet for you. Check out the colourful collection of chrysanthemums, daisies, roses, orchids and other exotic flowers. You can't say it better than with a bouquet from Highland Farms.",
-    "image" => "assets/images/store_flowers_web_crop.jpg",
-    "class" => "lightgreen"
-);
+$inside[] = array(     "title" => "Flowers",     "subtitle" => "In Full Bloom",     "description" => "Stop by our flower shop for potted plants and beautiful blooms. Ask our specially trained assistants to put together a customized bouquet.",     "image" =>
+"assets/images/store_flowers_web_crop.jpg",     "class" => "lightgreen" );
 
-$inside[] = array(
-    "title" => "Groceries",
-    "subtitle" => "Groceries From All Over",
-    "description" => "As the innovator of the ultimate one-stop supermarket, Highland Farms is committed to bringing you the widest range of groceries to make your shopping convenient and complete in one location, all while saving you more every day!
-    <br><br>
-    Highland Farms knows that different cultures have different needs. That's why you'll find one of Ontario's largest selections of international foods from around the world when you shop at Highland Farms. In addition to everyday grocery items, you can always expect to find that special item to suit your individual tastes.
-    <br><br>
-    It's just one more way that Highland Farms is committed to its customers!",
-    "image" => "assets/images/93472004.jpg",
-    "class" => "green"
-);
+$inside[] = array(     "title" => "Groceries",     "subtitle" => "Selection to suit every taste",     "description" => "As the innovator of the ultimate one-stop supermarket, we are committed to bringing you the widest range of groceries, all while saving you money every day.",     "image" => "assets/images/93472004.jpg",     "class" =>
+"green" );
 
-$inside[] = array(
-    "title" => "Meat",
-    "subtitle" => "Where Freshness Meats Value",
-    "description" => "When shopping for meat, there's nothing more reassuring than to see prime cuts of fresh meat, weighed and packaged right before your eyes. At Highland Farms, that's exactly what you'll find - premium grades of beef, pork, lamb, veal and game meat like rabbit and pheasant. You can also choose from a variety of our in-store made fresh sausages, shish-kabobs and ready-to-cook marinated meats. Our trained and experienced butchers will ensure you get exactly what you want, including great advice on the best way to cook your meat to optimize flavour and guarantee the mouth-watering texture that your family deserves.",
-    "image" => "assets/images/store_meat_web_crop.jpg",
-    "class" => "red"
-);
+$inside[] = array(     "title" => "Meat",     "subtitle" => "A Cut Above",     "description" => "At our meat department you'll get prime cuts of fresh meat, weighed and packaged right before your eyes. Our friendly staff will ensure you get exactly what you want, including invaluable cooking advice.",     "image"
+=> "assets/images/store_meat_web_crop.jpg",     "class" => "red" );
 
-$inside[] = array(
-    "title" => "Natural &amp; Wellness",
-    "subtitle" => "Natural and Organic Products",
-    "description" => "Highland Farms knows that offering natural and organic alternatives is important to our customers. That's why Highland Farms carries thousands of organic and natural items to help you decide for yourself what's best for you and your family. Everything from cereals, jams, snacks, coffee and tea, soya milk and soya ice creams, and no-sugar-added items, you'll find it easier, at Highland Farms!",
-    "image" => "assets/images/177555175.jpg",
-    "class" => "natural"
-);
+$inside[] = array(     "title" => "Natural &amp; Wellness",     "subtitle" =>
+"For Your Natural Lifestyle",     "description" => "We know that offering organic and natural choices is important to our customers. Everything from cereals to soya ice creams – you'll find hundreds of items at a great price.",     "image"
+=> "assets/images/177555175.jpg",     "class" => "natural" );
 
-$inside[] = array(
-    "title" => "Prepared Foods",
-    "subtitle" => "No Time to Cook? Visit Our Prepared Foods Counter",
-    "description" => "When you're running late and there's no time to cook, visit the Highland Farms prepared foods counter. Highland Farms cooks up delicious meals such as, chicken, ribs, pasta and a variety of sides, all ready to serve with the same low price and same great taste. What's for dinner tonight?",
-    "image" => "assets/images/store_prepared_web_crop.jpg",
-    "class" => "lightgrey"
-);
+$inside[] = array(     "title" => "Country Kitchen",     "subtitle" => "No Time to Cook? We're Here to Help",     "description" => "Don't worry if you're running a little late. Our talented cooks whip up fresh and delicious meals such as chicken, ribs, pasta and a variety of sides – all ready to serve at a low price and great taste. Let us take care of dinner tonight!",     "image" =>
+"assets/images/store_prepared_web_crop.jpg",     "class" => "lightgrey" );
 
-$inside[] = array(
-    "title" => "Produce",
-    "subtitle" => "The Freshest Produce",
-    "description" => "The freshness and quality of the produce at Highland Farms will amaze you. The size, selection and value is unsurpassed and time and again, shoppers are delighted when they visit the produce section at Highland Farms. That's because all of our fruits and vegetables are hand-picked for you every day at the Ontario Food Terminal from local and international farmers, bringing you the freshest produce at the best prices possible. ",
-    "image" => "assets/images/store_produce_web_crop.jpg",
-    "class" => "lightgreen"
-);
+$inside[] = array(     "title" => "Produce",     "subtitle" => "The Freshest Produce",     "description" => "The size, selection and value of our produce department is unsurpassed. Everything is hand-picked every day at the Ontario Food Terminal from local and international farmers, bringing you the freshest produce at the best prices possible.",     "image" => "assets/images/store_produce_web_crop.jpg",
+"class" => "lightgreen" );
 
-$inside[] = array(
-    "title" => "Sushi",
-    "subtitle" => "A Taste of Japan",
-    "description" => "In the mood for some Japanese? Highland Farms offers ready-made fresh sushi to satisfy your taste buds. Packaged and ready to eat, Highland Farms gives you a taste of Japan without the cost of expensive specialty restaurants.",
-    "image" => "assets/images/store_dairy_web_crop.jpg",
-    "class" => "black"
-);
-$inside[] = array(
-    "title" => "Fresh Poultry",
-    "subtitle" => "Fresh Poultry",
-    "description" => "",
-    "image" => "assets/images/store_poultry_web_crop.jpg",
-    "class" => "yellow"
-);
+$inside[] = array(     "title" => "Sushi",     "subtitle" => "Fresh and Ready to Roll",     "description" => "In the mood for some Japanese? We offer fresh sushi to satisfy your cravings. Packaged and ready to enjoy, Highland Farms gives you a taste of Japan without the cost of expensive specialty restaurants.",     "image" =>
+"assets/images/store_dairy_web_crop.jpg",     "class" => "black" ); $inside[]
+= array(     "title" => "Fresh Poultry",     "subtitle" => "Fresh Poultry",
+"description" => "",     "image" =>
+"assets/images/store_poultry_web_crop.jpg",     "class" => "yellow" );
 
 ?>
 </header>
