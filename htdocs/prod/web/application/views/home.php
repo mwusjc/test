aa1a2fd8a4809e2bbe5ce9037405833ab2591c52
Anirudh Swaminathan <aswaminathan@P15013.local>
2015-10-30 21:52:33 -0400

    bugfixes

--- htdocs/prod/web/application/views/home.php
+++ htdocs/prod/web/application/views/home.php
@@ -1,13 +1,13 @@
 <div id="carousel" class="carousel slide home" data-ride="carousel" data-interval="false" >
     <div class="carousel-inner" role="listbox">
-        <div class="item active responsive">
+        <a href="/recipes/408/Basil-Rice-Soup"  class="item active responsive">
             <img src="<?=site_url()?>/assets/images/home_carousel-1.jpg">
             <div class="carousel-caption" style="padding:40px 20px 15px 20px; width:800px;">
                 <img src="<?=site_url()?>/assets/images/icon_sizzle.png" style="float:right; width: 100px; margin: -42px 17px -35px -20px;"/><h1 style="font-size: 60px; font-family: 'Serifa';line-height:0.7em;">Bring home the sizzle</h1>
                 <h2 style="font-size:1.45em;">Pre-seasoned meats ready-to-grill at your local Highland Farms. Start fresh.</h2>
             </div>
-        </div>
-        <div class="item responsive">
+        </a>
+        <a  href="/flyer"class="item responsive">
             <img src="<?=site_url()?>/assets/images/home_carousel-2.jpg">
             <div class="carousel-caption save green">
                 <span class="price">
@@ -18,8 +18,8 @@
                     Milk-Fed Veal <br/>
                     Shoulder Blade</p>
             </div>
-        </div>
-        <div class="item responsive">
+        </a>
+        <a href="/flyer" class="item responsive">
             <img src="<?=site_url()?>/assets/images/home_carousel-3.jpg">
             <div class="carousel-caption feature teal">
                 <span class="price">
@@ -29,8 +29,8 @@
                 <p>Fresh <br/>
                     Tilapia Fillets</p>
             </div>
-        </div>
-        <div class="item responsive">
+        </a>
+        <a href="/flyer" class="item responsive">
             <img src="<?=site_url()?>/assets/images/home_carousel-4.jpg">
             <div class="carousel-caption feature teal">
                 <span class="price">
@@ -40,8 +40,8 @@
                 <p>Fresh <br/>
                     Tilapia Fillets</p>
             </div>
-        </div>
-        <div class="item responsive">
+        </a>
+        <a href="/flyer" class="item responsive">
             <img src="<?=site_url()?>/assets/images/home_carousel-5.jpg">
             <div class="carousel-caption feature teal">
                 <span class="price">
@@ -51,8 +51,8 @@
                 <p>Fresh <br/>
                     Tilapia Fillets</p>
             </div>
-        </div>
-        <div class="item responsive">
+        </a>
+        <a href="/flyer" class="item responsive">
             <img src="<?=site_url()?>/assets/images/home_carousel-6.jpg">
             <div class="carousel-caption feature teal">
                 <span class="price">
@@ -62,7 +62,7 @@
                 <p>Fresh <br/>
                     Tilapia Fillets</p>
             </div>
-        </div>
+        </a>
     </div>
     <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
