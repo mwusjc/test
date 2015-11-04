31fe915de5d5cb9cfb23354d2df809de6fd07f78
Anirudh Swaminathan <aswaminathan@P15013.local>
2015-11-04 11:39:43 -0500

    bugfixes

--- htdocs/prod/web/application/views/originals.php
+++ htdocs/prod/web/application/views/originals.php
@@ -15,7 +15,7 @@
         <div class='col-sm-7'>
             <h2>Fresh Gift Idea</h2>
             <p>
-                The Highland Farm Gift Card is the perfect gift for any occasion. Available in denominations from $25 to $100, they are sure to please friends and relatives who love to entertain. Pick one up at the checkout on your next visit.
+                The Highland Farm Gift Card is the perfect gift for any occasion. Available in denominations from $25 to $100, they are sure to please friends and relatives who love to entertain. Pick one up at the checkout on your next visit. 
             </p>
         </div>
     </div>
@@ -25,9 +25,9 @@
                 <img src='<?=sizeify( site_url('/assets/images/originals/originals_bags.jpg'), '370x260' )?>' class='border'>
         </div>
         <div class='col-sm-7'>
-            <h2>Exclusive To Us</h2>
+            <h2>Going Green is in the Bag</h2>
             <p>
-                Prepared with the finest ingredients and using time-tested recipes, every delicious Country Kitchen dish is a comforting celebration of food.
+               Do your part to reduce and reuse with our durable shopping bag. It holds double the groceries versus the traditional plastic bag.
             </p>
         </div>
     </div>
@@ -37,7 +37,7 @@
                 <img src='<?=sizeify( site_url('/assets/images/originals/originals_ck.jpg'), '370x260' )?>' class='border'>
         </div>
         <div class='col-sm-7'>
-            <h2>Fresh Gift Idea</h2>
+            <h2>Exclusive To Us</h2>
             <p>
            Prepared with the finest ingredients and using time-tested recipes, every delicious Country Kitchen dish is a comforting celebration of food. </p>
            <p><a href='<?=site_url("/country-kitchen")?>' class='btn green'>See what's cooking</a></p>
