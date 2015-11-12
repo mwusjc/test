f5f79c6625fbe9e9ccedcd8daaba84d4fa89458e
inaciob <binacio@live.ca>
2015-11-12 16:36:14 -0500

    Updated width of 4 homepage thumbnails on mobile

--- htdocs/prod/web/application/views/home.php
+++ htdocs/prod/web/application/views/home.php
@@ -110,25 +110,25 @@
     <hr class='extra-space'>
 
     <div class="row">
-        <div class="col-xs-6 col-sm-3">
+        <div class="col-xs-6 col-sm-3 home-thumbnail-grid">
             <a href='<?=site_url()?>visit-us'>
                 <img class="col-xs-12" src="<?=site_url()?>/assets/images/home_visit.jpg"/>
                 <p class="col-xs-12">Visit Us</p>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-3">
+        <div class="col-xs-6 col-sm-3 home-thumbnail-grid">
             <a href='<?=site_url()?>party-platters'>
                 <img class="col-xs-12" src="<?=site_url()?>/assets/images/home_pp.jpg"/>
                 <p class="col-xs-12">Party Platters And More</p>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-3">
+        <div class="col-xs-6 col-sm-3 home-thumbnail-grid">
             <a href='<?=site_url()?>about/highland-farms-originals'>
                 <img class="col-xs-12" src="<?=site_url()?>/assets/images/home_originals.jpg"/>
                 <p class="col-xs-12">Highland Farms Originals</p>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-3">
+        <div class="col-xs-6 col-sm-3 home-thumbnail-grid">
             <a href='<?=site_url()?>inside-store'>
                 <img class="col-xs-12" src="<?=site_url()?>/assets/images/home_inside-store.jpg" />
                 <p class="col-xs-12">Inside The Store</p>
