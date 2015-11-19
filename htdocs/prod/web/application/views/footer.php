fe3632a06b8421a364ac4ee93f76b603051d4ad4
inaciob <binacio@live.ca>
2015-11-19 11:38:59 -0500

    Added Google Tag Manager tracking code

--- htdocs/prod/web/application/views/footer.php
+++ htdocs/prod/web/application/views/footer.php
@@ -54,5 +54,15 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-56448531-1', 'auto');
 ga('send', 'pageview');
 </script>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K5FQFX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){wl=wl||[];wl.push(
+{'gtm.start': new Date().getTime(),event:'gtm.js'}
+);var f=d.getElementsByTagName(s)0,
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K5FQFX');</script>
+<!-- End Google Tag Manager -->
 </body>
 </html>
