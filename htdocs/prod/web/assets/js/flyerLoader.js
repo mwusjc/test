05761e767526ee153ee6788d99d8abf89a198806
inaciob <binacio@live.ca>
2015-12-02 10:48:00 -0500

    Moved all current flyer images from category specific folders to within flyer-images folder

--- htdocs/prod/web/assets/js/flyerLoader.js
+++ htdocs/prod/web/assets/js/flyerLoader.js
@@ -109,7 +109,8 @@ var fl = {
 				html += 	'			<div class="modal-body" data-category="'+ prod.category +'" >'
 				html += 	'				<div class="row">'
 				html += 	'					<div class="col-xs-12 col-sm-6 text-center">'
-				html += 	'						<img class="image productPopupImage" src="/assets/images/flyer-images/'+prod.category+'/'+prod.image+'">'
+				// html += 	'						<img class="image productPopupImage" src="/assets/images/flyer-images/'+prod.category+'/'+prod.image+'">'
+				html += 	'						<img class="image productPopupImage" src="/assets/images/flyer-images/'+prod.image+'">'
 				html += 	'					</div>'
 				html += 	'					<div class="col-xs-12 col-sm-6">'
 				html += 	'						'+(prod.comments=='save'?'<h3 class="comment">save more!</h3>':"");
@@ -248,7 +249,8 @@ var fl = {
 				}
 				html+=  	'<div class="row" data-category="'+prod.category+'" data-brand="'+brandstring+'">'
 				html+=	    '	<div class="col-xs-12 col-sm-3 text-center">'
-				html+=	    '		<img class="image" src="/assets/images/flyer-images/'+prod.category+'/'+prod.image+'">'
+				// html+=	    '		<img class="image" src="/assets/images/flyer-images/'+prod.category+'/'+prod.image+'">'
+				html+=	    '		<img class="image" src="/assets/images/flyer-images/'+prod.image+'">'
 				html+=	    '	</div>'
 				html+=	    '	<div class="col-xs-12 col-sm-9">'
 				html+=	    '		'+(prod.comments=='save'?'<h3 class="comment">save more!</h3>':"");
