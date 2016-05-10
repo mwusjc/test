8558b7bdaef78cd14b49d280148030586aaba762
Brian Inacio <brian.inacio@stjoseph.com>
2016-05-10 15:11:43 -0400

    Test #195 adding SQL script commands to update order of platters categories

--- db/0195-platters-categories-reorder.sql
+++ db/0195-platters-categories-reorder.sql
@@ -1,3 +1,7 @@
 -- --------------------------------------------------------
 -- https://sjc-istudio.assembla.com/spaces/www-highlandfarms-ca/tickets/new_cardwall#ticket:195
 -- --------------------------------------------------------
+USE `webdev_hlf`;
+UPDATE platters_categories SET platters_categories.OrderID=REPLACE(orderID, '3', '2') WHERE platters_categories.id=3;
+UPDATE platters_categories SET platters_categories.OrderID=REPLACE(orderID, '2', '3') WHERE platters_categories.id=1;
+UPDATE platters_categories SET platters_categories.Name=REPLACE(name, 'Deli, Cheese, Vegetables & Seafood', 'Deli, Cheese & Vegetables') WHERE platters_categories.id=1;
