58c8110a6af9cfc6b32c161ca2e526720188a124
Brian Inacio <brian.inacio@stjoseph.com>
2016-05-16 15:05:44 -0400

    Re #214 adding new samosas platter info to local DB with corresponding image asset

---
+++ db/0214-platters-add-new-samosas-empanadas.sql
@@ -0,0 +1,5 @@
+-- --------------------------------------------------------
+-- https://sjc-istudio.assembla.com/spaces/bx_hUmG88r5AL4dmr6CpXy/tickets/new_cardwall?default_list_cardwall=milestone:9885793#ticket:214
+-- --------------------------------------------------------
+USE `webdev_hlf`;
+INSERT INTO platters (Name, CategoryID, Price, Price2, Price3, Image, `TimeStamp`, UserID, Status, Quantity, Quantity2, Quantity3, Description) VALUES('Mini Samosas & Dip', 3, 39.99, '', '', '/assets/images/pp_pop_samosas.jpg', 0, 0, 'enabled', '40 pcs. Serves 8 - 12', NULL, NULL, 'Savour our potato-filled samosas with your choice of dip: Chunky Blue Cheese, Roasted Red Pepper, Avocado Hummus or our favourite, Artichoke & Asiago.');
