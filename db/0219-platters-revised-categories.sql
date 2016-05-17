d92eea405bbd1a711f11ecbeb58abcaf9a491921
Brian Inacio <brian.inacio@stjoseph.com>
2016-05-17 10:07:09 -0400

    Re #219 adding new perogies platter

--- db/0219-platters-revised-categories.sql
+++ db/0219-platters-revised-categories.sql
@@ -9,3 +9,4 @@ UPDATE platters_categories SET platters_categories.Name=REPLACE(name, 'Special O
 UPDATE platters_categories SET platters_categories.OrderID=REPLACE(orderID, '7', '6') WHERE platters_categories.id=6;
 UPDATE platters_categories SET platters_categories.OrderID=REPLACE(orderID, '5', '7') WHERE platters_categories.id=4;
 UPDATE platters_categories SET platters_categories.OrderID=REPLACE(orderID, '4', '6') WHERE platters_categories.id=2;
+INSERT INTO platters (Name, CategoryID, Price, Price2, Price3, Image, `TimeStamp`, UserID, Status, Quantity, Quantity2, Quantity3, Description) VALUES('Perogies & Dip', 3, 34.99, '', '', 'images/pp_pop_perogies.jpg', 0, 0, 'enabled', '40 pcs. Serves 8 - 12', NULL, NULL, 'Golden brown and crispy on the outside, each perogi is stuffed with potato and cheese goodness. Serve it with your favourite dip: Chunky Blue Cheese, Roasted Red Pepper, Artichoke & Asiago or Avocado Hummus.');
