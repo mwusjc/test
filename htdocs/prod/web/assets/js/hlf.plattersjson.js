3d346cafaf13b7899f95a59ca1236e67271ff07a
Brian Inacio <brian.inacio@stjoseph.com>
2016-06-29 09:52:14 -0400

    Re #247 moving  platters JS logic into plattersjson file, updating global footer to include platters logic file

--- htdocs/prod/web/assets/js/hlf.plattersjson.js
+++ htdocs/prod/web/assets/js/hlf.plattersjson.js
@@ -3,119 +3,96 @@
     init: function(data) {
       this.filterListener();
       this.drawList(data);
+      this.setListeners();  
     },
-
-    getCategories: function(data) {
-
-      var categories = [];
-
-      //iterate over each platter
-      $.each(data, function(key,item) {
-
-        //iterate over each category in the recipe
-        $.each(item.categories, function(key2,category) {
-
-          if (!(hlf.platters.searchCategories(category, categories))) {
-            categories.push(category);
-            mapping = {
-              "_TITLE_" : category.title,
-              "_SLUG_" : category.slug
-            };
-            html = hlf.drawTemplate("#tpl-category", mapping);
-            $('.categories-list').append(html);
-          }
-        });
-      });
-
-    },
-
-    sortCategories: function(selector) {
-      var mylist = $(selector);
-      var listitems = mylist.children('li').get();
-      listitems.sort(function(a, b) {
-         return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
-      })
-      $.each(listitems, function(index, item) { mylist.append(item); });
-    },
-
-    // slightly modified version of: http://phpjs.org/functions/nl2br/
-    nl2br: function(str) {
-      var breakTag = '<br>';
-
-      return (str + '')
-        .replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
-    },
-
-    filterListener: function() {
-      var that = this;
+    
+    filterListener: function() {  
+      var that = this; 
       $('[data-filter-id]').on("click", function() {
-        that.filterCategory( hlf.data.recipes, $(this).data('filter-id') );
+         that.filterCategory( hlf.data.platters, $(this).data('filter-id') );
       });
-
-      $('[data-filter-search]').on("change keyup", function() {
-         that.filterSearch( hlf.data.recipes, $(this).val() );
-      });
-
     },
 
-    filterCategory : function(data,filter) {
+    filterCategory : function(data,filter) {    
       var filtered = {};
       $.each(data, function(key,item) {
-
-        // check if item category that was clicked (i.e. poultry) is in the list of categories in
-        // the current item
-        if(hlf.recipes.searchCategories(filter, item.categories)) {
-          filtered[key] = item;
-        }
+         if(item.CategoryID == filter) {
+          filtered[key] = item;   
+         } 
       });
       this.drawList(filtered);
       $('input.search').val(null);
+    }, 
+
+    togglePopup: function(id) {
+      $('#detailModal.otu').remove();  // remove all modal instances (one time use)
+      var item = hlf.data.platters[id];
+      mapping = { 
+          "{IMG}" : "<?=site_url() ?>"+"assets/"+item.Image,
+          "{QTY}" : (item.Quantity ? item.Quantity : ''), 
+          "{QTY2}" : (item.Quantity2 ? item.Quantity2 : ''), 
+          "{QTY3}" : (item.Quantity3 ? item.Quantity3 : ''), 
+          "{QTY_TYPE}" : (item.Qty_type ? item.Qty_type : ''), 
+          "{TITLE}": item.Name, 
+          "{SUBTITLE}": (item.Subtitle ? item.Subtitle : ''), 
+          "{DESCRIPTION}": item.Description, 
+          "{ID}": item.id,
+          "{PRICE}": (item.Price ? "$" + item.Price : ''),
+          "{PRICE2}": (item.Price2 ? "$" + item.Price2 : ''),
+          "{PRICE3}": (item.Price3 ? "$" + item.Price3 : ''),
+          "{VISUALORDER}": (item.VisualOrder ? item.VisualOrder : '')
+      };
+      html = hlf.drawTemplate("#tpl-product-modal", mapping);
+
+      $('body').append(html);
+      $('#detailModal').modal('show');  
     },
 
-    searchCategories: function(nameKey, myArray) {
-      for (var i=0; i < myArray.length; i++) {
-        if (myArray[i].slug === nameKey) {
-            return true;
-        }
-      }
-      return false;
-    },
-
-    filterSearch: function(data, filter) {
-      var filtered = {};
-      $.each(data, function(key,item) {
-         if(item.title.toLowerCase().indexOf(filter.toLowerCase()) > -1) {
-        filtered[key] = item;
-         }
+    setListeners: function() {
+      var that = this;
+      $(document).on('click', '[data-toggle-details]', function(e) {
+          e.preventDefault();
+          that.togglePopup( $(this).data("toggle-details") ); 
       });
-      this.drawList(filtered);
-      $('li').removeClass('active');
+            
+      $('body').on("click", '[data-add-cart]', function(e) {
+         e.preventDefault(); 
+         var id = $(this).data("add-cart");
+         
+         $.post('/shopping/add/', {"id": id},  function(response) {
+            console.log(response); 
+         }).fail(function() {
+            alert("Unfortunately something went wrong! Please try again."); 
+         });
+         
+      });
     },
 
-    drawList: function(data) {
-      $('.recipes-container').html(' ');
+    drawList: function(data) { 
+      $('.platters-container').html(' ');
       $.each(data, function(key,item) {
-        mapping = {
-          "_THUMBNAIL_" : item.thumbnail,
-          "_TITLE_" : item.title,
-          "_SLUG_" : item.slug
-        };
-        html = hlf.drawTemplate("#tpl-recipe-listing", mapping);
-        $('.recipes-container').append(html);
+          mapping = { "_IMAGE_" : item.Image, "_TITLE_" : item.Name, "_ID_" : key, "_PRICE_" : item.Price, "_DESCRIPTION_": item.Description, "_VISUALORDER_": item.VisualOrder };
+          html = hlf.drawTemplate("#tpl-platter-listing", mapping);
+          $('.platters-container').append(html);
       });
-      if( $('.recipe').length == 0) {
+      if( $('.platter').length == 0) {
         $('.no-results-found').removeClass('hidden');
-      } else {
+      } 
+      else {
         $('.no-results-found').addClass('hidden');
       }
-      if($('.recipe').length > 9) this.pajinate(".pajinate");
-      else $('.page_navigation').html(' ');
-       $("img.lazy").lazyload({
+      if($('.platter').length > 9) {
+        this.pajinate(".pajinate");
+      }
+      else {
+        $('.page_navigation').html(' ');
+      }
+      $("img.lazy").lazyload({
           placeholder : "img/grey.gif",
           effect      : "fadeIn"
-       });
-       $(window).scroll(); // won't load images right away without this line
+      });   
     },
+
     pajinate: function(ele) {
       $(ele).pajinate({
         'items_per_page': 9,
