ed71bc347221251b4ff5edc3d180dbafb6ef037c
Matt Graham <matthew@mattgraham.ca>
2016-07-29 11:46:39 -0400

    Oops DB fix

--- htdocs/prod/web/application/config/database.php
+++ htdocs/prod/web/application/config/database.php
@@ -97,29 +97,7 @@ $query_builder = TRUE;
 //	'save_queries' => TRUE
 //);
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'Burdon68',
-	'database' => 'webdev_hlf',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-/* stinson server
+// stinson server
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => '167.88.112.253',
@@ -141,4 +119,3 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-*/
