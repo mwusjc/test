52635529f36a4d8b8948b8963c9dac20f51a6820
Brian Inacio <brian.inacio@stjoseph.com>
2016-10-24 09:55:24 -0400

    Test #366 created script to remove specified breads from database for upcoming deployment

---
+++ db/0366-country-kitchen-breads-updates.sql
@@ -0,0 +1,7 @@
+-- --------------------------------------------------------
+-- https://app.assembla.com/spaces/www-highlandfarms-ca/tickets/realtime_cardwall?ticket=366
+-- --------------------------------------------------------
+USE `webdev_hlf`;
+DELETE FROM products WHERE products.id = 1;
+DELETE FROM products WHERE products.id = 2;
+DELETE FROM products WHERE products.id = 3;
