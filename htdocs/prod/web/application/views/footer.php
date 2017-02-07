edfcb60da443bb4a8b2b1758c7c1a237812235d3
Michael Wu <michael.wu@P14008-MWu-10-2-101-105.vlan-2.stjosephcontent.com>
2017-02-07 16:26:30 -0500

    Re #456 - inserted adwords remarketing tag in footer.php

--- htdocs/prod/web/application/views/footer.php
+++ htdocs/prod/web/application/views/footer.php
@@ -52,5 +52,23 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-56448531-1', 'auto');
 ga('send', 'pageview');
 </script>
+<!-- Google Code for Remarketing Tag -->
+<!--
+Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
+-->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 866265235;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/866265235/?guid=ON&amp;script=0"/>
+</div>
+</noscript>
 </body>
 </html>
