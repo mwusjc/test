1f0fb03f8616c3c3b507921cffaa27397a96a726
Michael Wu <michael.wu@P14008-MWu-10-2-101-105.vlan-2.stjosephcontent.com>
2017-03-06 09:06:45 -0500

    Re #455 - added multiple breakpoints for text and form elements in assets/css/vaughan.css

---
+++ htdocs/prod/web/application/views/vaughan-2.php
@@ -0,0 +1,91 @@
+</header>
+
+<main class='landing'>
+  <div class="row">
+    <div class="col-md-7"><h4 style="color: #00a64f; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">Grand Opening Countdown</h4></div>
+    <div class="col-md-5">PLACEHOLDER</div>
+  </div>
+  <div class="row" style="background:#00a64f; border-radius: 30px 30px 0 0;">
+    <div class="col-xs-12 col-sm-12"><h4 style="color:white; font-size: 60px; font-family: 'HelveticaNeueLT-Bold'; padding-top: 30px" align="center">Here we grow again!</h4></div>
+  </div>
+  <div class="row" style="background:#00a64f;">
+    <div class="col-xs-12 col-sm-12">
+    <span style="color:white; font-size: 20px; font-family: 'HelveticaNeueLT-Bold';" align="center">Join us Mar. 31 at 10am for our Grand Opening Celebration</span>
+    </div>
+  </div>
+  <div class="row" style="background:#00a64f;">
+    <div class="col-md-7">
+      <h4 style="color:white; font-size: 20px; font-family: 'SerifaBT-Light';" >Dufferin St. at Major MacKenzie Dr. W</h4>
+    </div>
+    <div class="col-md-5">
+      <input type="submit" aria-label="Submit" value="+ add to your calendar" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" style="background-color: #f99d1c; margin:0 0 40px 0">
+    </div>
+  </div>
+  <div class="row" style="background:#f99d1c;">
+    <div class="col-xs-12 col-sm-12"><h3 style="color:white; font-size: 40px; font-family: 'HelveticaNeueLT-Bold'; padding-top: 30px" align="center">Come celebrate our new store</h3></div>
+  </div>
+  <div class="row" style="background:#f99d1c;">
+    <div class="col-xs-12 col-sm-12">
+      <h4 align="left" style="color:white; font-family: 'HelveticaNeueLT-Bold'; margin: 0"><span style="font-size: 20px;">Ribbon-Cutting</span> <span style="font-size: 16px">with the Honourable Maurizio Bevilacqua, Mayor of Vaughan</span></h4><br>
+      <h4 align="left" style="color:white; font-family: 'HelveticaNeueLT-Bold'; margin: 0"><span style="font-size: 20px;">Special donation Presentation</span> <span style="font-size: 16px">to support the Mackenzie Vaughan Hospital Foundation</span></h4>
+    </div>
+  </div>
+  <div class="row" style="background:#f99d1c;">
+    <div class="col-md-3" style="background: #00a64f; height: 200px; width: 200px; border-radius: 100px; text-align: center; margin-left: -30px; margin-top: 30px"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px; position: relative;top: 50%;transform: translateY(-50%);">Plus</h4></div>
+    <div class="col-md-9" style="margin-top: 25px; margin-bottom: 25px"">
+      <div class="row">
+        <div class="col-xs-3 col-md-3"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px;">FREE</h4></div>
+        <div class="col-xs-12 col-md-8"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">$25 Gift Card to <br>first 200 customers</h4></div>
+      </div>
+      <div class="row">
+        <div class="col-xs-3 col-md-3"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px;">FREE</h4></div>
+        <div class="col-xs-12 col-md-8"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">Reusable Shopping <br>Bag with $25 purchase</h4></div>
+      </div>
+      <div class="row">
+        <div class="col-xs-3 col-md-3"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px;">FREE</h4></div>
+        <div class="col-xs-12 col-md-8"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">food sampling and Complimentary <br>Coffee &amp; Cake in-store</h4></div>
+      </div>
+    </div>
+  </div>
+  <div class="row" style="background:#f99d1c;">
+    <hr>
+    <div class="col-xs-12 col-sm-12"><h3 style="color:white; font-size: 40px; font-family: 'HelveticaNeueLT-Bold'; padding-top: 30px" align="center">Get your Grand Opening Coupon</h3></div>
+  </div>
+  <div class="row" style="background:#f99d1c;">
+    <hr>
+    <div class="col-xs-12 col-md-8"><h1 style="color:white; font-size: 120px; font-family: 'HelveticaNeueLT-Bold';" align="center">SAVE $10</h1></div>
+    <div class="col-xs-6 col-md-4"><h1 style="color:white; font-size: 24px; font-family: 'HelveticaNeueLT-Bold';" align="left">when you<br>spend $50 or<br>more at our<br>Vaughan store</h1></div>
+  </div>
+  <div class="row" style="background:#f99d1c;">
+    <div class="col-md-7"><p style="color:white; font-size: 12px; font-family: 'HelveticaNeueLT-Condensed'; line-height: 1" align="left">Valid March 31, 2017 only at our Vaughan location (9940 Dufferin St.) when you spend $150 or more before taxes. Coupon must be surrendered at time of purchase. Mobile technology redemption not available. Coupon limited to one per customer. Photocopies will not be accepted. No cash value. </p></div>
+    <div class="col-md-5"><input type="submit" aria-label="Submit" value="Sign up for your coupon >" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" style="background-color: #00a64f; margin:0 auto"></div>
+  </div>
+  <div class="row" style="background:white; margin-top: 30px"> 
+    <h1 style="color: #00a64f; font-size: 120px; font-family: 'HelveticaNeueLT-Bold'"">You'll love our Vaughan store!</h1>
+  </div>
+  <div class="row"> 
+    <img src="<?=site_url()?>/assets/media/vaughan/store-front.jpg" style="width: 100%; height: auto; z-index:0.5">
+    <div class="col-md-3" style="background: #f99d1c; float: right; height: 200px; width: 200px; border-radius: 100px; text-align: center; margin-right: -30px; margin-top: -90%"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 36px; position: relative;top: 50%;transform: translateY(-50%); z-index:2">FRESH<br>&amp; NEW</h4></div>
+    <div class="row">
+      <div class="col-md-4" style="margin-top: -120px">
+        <p style="color:white; font-size: 16px; font-family: 'SerifaBT-Light'; line-height: 1; padding-left: 20px">Redesigned from top to <br>bottom, with a bright layout, <br>colour-coded sections and <br>wide aisles, our new store <br>will make your shopping <br>experience more enjoyable!</p>
+      </div>
+    </div>
+  </div>
+</main>
+<div class="container-fluid">
+  <div class="row">
+    <img src="<?=site_url()?>/assets/media/vaughan/deli-poultry.jpg" style="width: 100%; height: auto;">
+  </div>
+</div>
+<main style="margin-right: auto; margin-left: auto;">
+  <div class="row col-md-12 col-xs-12 col-lg-12">
+    <h1 style="color: #f99d1c; font-size: 100px; font-family: 'HelveticaNeueLT-Bold'"">Our fresh new look</h1>
+  </div>
+  <div class="row col-md-12 col-xs-12 col-lg-12">
+    <p style="color:black; font-size: 20px; font-family: 'SerifaBT-Light';" >Diciis anduntent qui cum autet illenim que verio et voluptaspere et ex excerfe rferspidit eres sed quis et et ditas dolorum que volupta nimus as autemquo mintem quossit pernatectur</p>
+  </div>
+  <div class="row col-md-12 col-xs-12 col-lg-12">
+    <img src="<?=site_url()?>/assets/media/vaughan/produce.jpg" style="width: 100%; height: auto;">
+  </div>
+</main>
--- htdocs/prod/web/application/views/vaughan.php
+++ htdocs/prod/web/application/views/vaughan.php
@@ -1,151 +1,55 @@
 </header>
 
-<main class='home'>
-	<div class="row countdown">
-		<div class="col-xs-12 col-sm-12">
-      <div style="background:#00a64f; padding:12px 15px; margin:0; border-radius: 30px 30px 0 0; height: 450px;">
-        <h4 style="color:white; font-size: 24px; font-family: 'HelveticaNeueLT-Bold'; padding-top: 30px" align="center">Something’s coming just around the corner</h4>
-        <h4 style="color:#f99d1c; font-size: 20px; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;" align="center">#hfstartfresh</h4>
-        <style>
-          #clockdiv {
-            margin-top:30px; margin-bottom:20px;
-          }
+<link rel="stylesheet" href="<?=site_url()?>assets/css/vaughan.css">
 
-          .smalltext {
-            color:white; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; text-align: left;
-          }
-          
-          .clocknumber {
-            color:#f99d1c; font-family: 'HelveticaNeueLT-Bold';
-          }
-          @media (min-width: 644px) {
-            .clocknumber {
-              font-size: 95px;
-            }
-            #clockdiv {
-              margin-top:60px; margin-bottom:20px;
-            }
-            .smalltext {
-              font-size: 18px; 
-            }
-          }
-          @media (min-width: 451px) and (max-width: 643px) {
-            .clocknumber {
-              font-size: 62px;
-            }
-          }
-          @media (min-width: 356px) and (max-width: 450px) {
-            .clocknumber {
-              font-size: 48px;
-            }
-            .smalltext {
-              font-size: 12px;
-            }
-            #clockdiv {
-              padding-top:15px; padding-bottom:10px;
-            }
-          }
-          @media (max-width: 355px) {
-            .clocknumber {
-              font-size: 36px;
-            }
-            .smalltext {
-              font-size: 11px;
-            }
-          }
-        </style>
-        <div id="clockdiv" align="center">
-            <div style="display:inline-block;">
-              <span class="days clocknumber">00:</span>
+<main class='landing'>
+	<div class="row countdown">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <div class="bg-primary text-area text-center">
+        <h1>Something’s coming just around the corner</h1>
+        <div id="clockdiv">
+            <div class="numbercontainer">
+              <span class="days clocknumber orange-text heading-text">00:</span>
               <div class="smalltext">Days</div>
             </div>
-            <div style="display:inline-block;">
-              <span class="hours clocknumber">00:</span>
+            <div class="numbercontainer">
+              <span class="hours clocknumber orange-text heading-text">00:</span>
               <div class="smalltext">Hours</div>
             </div>
-            <div style="display:inline-block;">
-              <span class="minutes clocknumber">00:</span>
+            <div class="numbercontainer">
+              <span class="minutes clocknumber orange-text heading-text">00:</span>
               <div class="smalltext">Minutes</div>
             </div>
-            <div style="display:inline-block;">
-              <span class="seconds clocknumber">00:</span>
+            <div class="numbercontainer">
+              <span class="seconds clocknumber orange-text heading-text">00:</span>
               <div class="smalltext">Seconds</div>
             </div>
         </div>
-        <script>
-        function getTimeRemaining(endtime) {
-          var t = Date.parse(endtime) - Date.parse(new Date());
-          var seconds = Math.floor((t / 1000) % 60);
-          var minutes = Math.floor((t / 1000 / 60) % 60);
-          var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-          var days = Math.floor(t / (1000 * 60 * 60 * 24));
-          return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-          };
-        }
-
-        function initializeClock(id, endtime) {
-          var clock = document.getElementById(id);
-          var daysSpan = clock.querySelector('.days');
-          var hoursSpan = clock.querySelector('.hours');
-          var minutesSpan = clock.querySelector('.minutes');
-          var secondsSpan = clock.querySelector('.seconds');
-
-          function updateClock() {
-            var t = getTimeRemaining(endtime);
-
-            daysSpan.innerHTML = t.days+":";
-            hoursSpan.innerHTML = ('0' + t.hours).slice(-2)+":";
-            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2)+":";
-            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-            if (t.total <= 0) {
-              clearInterval(timeinterval);
-            }
-          }
-
-          updateClock();
-          var timeinterval = setInterval(updateClock, 1000);
-        }
-
-        var deadline = 'March 31 2017 10:00:00 GMT-0500';
-        initializeClock('clockdiv', deadline);
-        </script>
-        <div class="subscribe col-xs-12 col-sm-10 desktop" style="padding: 0px; top: 20px; margin: 0 auto; float: none; height:100px" align="center">
-            <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" align="center" style="margin: 0 auto;">
-                <input type="email" value="" name="EMAIL" class="col-xs-12 col-sm-12" id="mce-EMAIL" aria-label="email" placeholder="Enter your email to stay tuned" aria-required="true" required style="width:90%">
+        <div class="col-xs-10 col-sm-10 col-md-10 vaughan-subscribe desktop">
+            <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                <input type="email" value="" name="EMAIL" class="col-xs-12 col-sm-12 email" id="mce-EMAIL" aria-label="email" placeholder="Enter your email to stay tuned" aria-required="true" required>
                 <div style="position: absolute; left: -5000px;"><input type="text" name="b_a574aa827269d018202389912_792b79af52" tabindex="-1" value="" placeholder="Your e-mail address"></div>
-                <input type="submit" aria-label="Submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email sticky right" style="background-color: #f99d1c;">
+                <input type="submit" aria-label="Submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email sticky right">
             </form>
         </div>
-        <div class="subscribe col-xs-12 col-sm-10 mobile" style="padding: 0px; top: 20px; margin: 0 auto; float: none;" align="center">
-            <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" align="center" style="margin: 0 auto;">
-                <input type="email" value="" name="EMAIL" class="col-xs-12 col-sm-12" id="mce-EMAIL" aria-label="email" placeholder="Enter your email to stay tuned" aria-required="true" required style="width:100%">
+        <div class="vaughan-subscribe mobile">
+            <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" aria-label="email" placeholder="Enter your email to stay tuned" aria-required="true" required >
                 <div style="position: absolute; left: -5000px;"><input type="text" name="b_a574aa827269d018202389912_792b79af52" tabindex="-1" value="" placeholder="Your e-mail address"></div>
                 <br>
-                <input type="submit" aria-label="Submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" style="background-color: #f99d1c; margin:0 0 40px 0">
+                <input type="submit" aria-label="Submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email">
             </form>
         </div>
-        <div align="center">
-          <style>
-            .social-icon {
-              height: 32px;
-              width: 32px;
-              margin: 2.5px;
-              display: inline-block;
-            }
-          </style>
-          <a href="https://www.facebook.com/HighlandFarmsON/" alt="facebook" class="social-icon" style="background-image:url(<?=site_url()?>/assets/media/social/social-icon-facebook-orange.png); background-size: cover"></a>
-          <a href="https://twitter.com/highlandfarmson" alt="twitter" class="social-icon" style="background-image:url(<?=site_url()?>/assets/media/social/social-icon-twitter-orange.png); background-size: cover"></a>
-          <a href="https://www.instagram.com/highlandfarms_on/" alt="instagram" class="social-icon" style="background-image:url(<?=site_url()?>/assets/media/social/social-icon-instagram-orange.png); background-size: cover"></a>
+        <div class="col-xs-12 col-sm-12 social-icons">
+          <a href="http://www.facebook.com/sharer.php?s=100&p[url]=http://hfstartfresh.ca" alt="facebook" style="background-image:url(<?=site_url()?>/assets/media/social/social-icon-facebook-orange.png);"></a>
+          <a href="http://twitter.com/intent/tweet?text=A+fresh+new+Highland+Farms+is+coming+to+Vaughan&hashtags=hfstartfresh" alt="twitter" style="background-image:url(<?=site_url()?>/assets/media/social/social-icon-twitter-orange.png);"></a>
+          <a href="https://www.instagram.com/highlandfarms_on/" alt="instagram" style="background-image:url(<?=site_url()?>/assets/media/social/social-icon-instagram-orange.png);"></a><br>
+          <span class="orange-text hashtag">#hfstartfresh</h4>
         </div>
       </div>
 		</div>
 	</div>
 
 </main>
-
+<script type="text/javascript" async src="<?=site_url()?>/assets/js/vaughan-countdown.js"></script>
+<script type="text/javascript" async src="https://platform.twitter.com/widgets.js"></script>
