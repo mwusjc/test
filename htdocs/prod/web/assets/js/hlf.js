868c491139ab2e7c2de9fc7c060c50e0afd72a20
Sean Macdonald <sean@crazyhorsecoding.com>
2017-03-07 16:38:11 -0500

    eliminate console errors

--- htdocs/prod/web/assets/js/hlf.js
+++ htdocs/prod/web/assets/js/hlf.js
@@ -1,11 +1,28 @@
-function replaceAll(find, replace, str) {
-  return str.replace(new RegExp(find, 'g'), replace);
-}
+/**
+ *  @description: render templates or <span /> if sanity tests don't pass
+ **/
 
+var isArray = jQuery.isArray;
+function isObject(thing) {
+    var r = false;
+    if (Object.prototype.toString.call(thing) === '[object Object]') {
+        r = true;
+    }
+    return r;
+}
+function replaceAll(find, replace, str) {
+    var r = str;
+    if (typeof str === 'string') {
+        return str.replace(new RegExp(find, 'g'), replace);    
+    }
+    return r;
+}
 hlf.drawTemplate = function(tpl, replace) {
-  $template = $(tpl).html();
-  $.each(replace, function(key,item) {
-    $template = replaceAll(key,item,$template);
-  });
-  return $template;
+    $template = $(tpl).html() || '<span />';
+    if (isObject(replace) || isArray(replace)) {
+        $.each(replace, function(key,item) {
+            $template = replaceAll(key,item,$template);
+        });
+    }
+    return $template;
 }
