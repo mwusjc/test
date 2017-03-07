357e0c840088a1156fe1f6aa74cdc0eb0b9b74de
Michael Wu <michael.wu@P14008-MWu-10-2-101-105.vlan-2.stjosephcontent.com>
2017-03-07 11:49:23 -0500

    Re #487 - relocated GA code from footer to head

--- htdocs/prod/web/application/views/footer.php
+++ htdocs/prod/web/application/views/footer.php
@@ -55,14 +55,6 @@
     carousel.init(carousel.currentTime);
   });
 </script>
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-56448531-1', 'auto');
-ga('send', 'pageview');
-</script>
 <!-- Google Code for Remarketing Tag -->
 <!--
 Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
