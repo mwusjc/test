ffb0f6e11d9f3d9d1bc41ad987e334558f0150ba
Michael Wu <michael.wu@P14008-MWu-10-2-101-105.vlan-2.stjosephcontent.com>
2017-03-10 14:44:28 -0500

    Re #455 - updated main landing page

--- htdocs/prod/web/application/views/vaughan-2.php
+++ htdocs/prod/web/application/views/vaughan-2.php
@@ -1,91 +1,150 @@
 </header>
 
-<main class='landing'>
-  <div class="row">
-    <div class="col-md-7"><h4 style="color: #00a64f; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">Grand Opening Countdown</h4></div>
-    <div class="col-md-5">PLACEHOLDER</div>
+<link rel="stylesheet" href="<?=site_url()?>assets/css/vaughan-2.css">
+
+<main class='landing-2'>
+  <div class="row countdown">
+    <div class="title col-md-8"><h1>Grand Opening Countdown</h1></div>
+    <div id="clockdiv" class="col-md-4">
+      <div class="numbercontainer">
+        <h1 class="seconds clocknumber orange-text heading-text">00:</h1>
+        <div class="smalltext">Seconds</div>
+      </div>
+      <div class="numbercontainer">
+        <h1 class="minutes clocknumber orange-text heading-text">00:</h1>
+        <div class="smalltext">Minutes</div>
+      </div>
+      <div class="numbercontainer">
+        <h1 class="hours clocknumber orange-text heading-text">00:</h1>
+        <div class="smalltext">Hours</div>
+      </div>
+      <div class="numbercontainer">
+        <h1 class="days clocknumber orange-text heading-text">00:</h1>
+        <div class="smalltext">Days</div>
+      </div>
+     </div>
   </div>
-  <div class="row" style="background:#00a64f; border-radius: 30px 30px 0 0;">
-    <div class="col-xs-12 col-sm-12"><h4 style="color:white; font-size: 60px; font-family: 'HelveticaNeueLT-Bold'; padding-top: 30px" align="center">Here we grow again!</h4></div>
-  </div>
-  <div class="row" style="background:#00a64f;">
+  <div class="row box orange rounded intro">
+    <div class="col-xs-12 col-sm-12"><h1>Join us Friday,<br>
+    March 31 at 10am for<br>
+    our Grand Opening!</h1></div>
     <div class="col-xs-12 col-sm-12">
-    <span style="color:white; font-size: 20px; font-family: 'HelveticaNeueLT-Bold';" align="center">Join us Mar. 31 at 10am for our Grand Opening Celebration</span>
+    <span style="font-family: 'SerifaBT-Light';" align="center">Dufferin St. &amp; Major MacKenzie Dr. W.</span>
     </div>
   </div>
-  <div class="row" style="background:#00a64f;">
-    <div class="col-md-7">
-      <h4 style="color:white; font-size: 20px; font-family: 'SerifaBT-Light';" >Dufferin St. at Major MacKenzie Dr. W</h4>
-    </div>
-    <div class="col-md-5">
-      <input type="submit" aria-label="Submit" value="+ add to your calendar" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" style="background-color: #f99d1c; margin:0 0 40px 0">
-    </div>
-  </div>
-  <div class="row" style="background:#f99d1c;">
-    <div class="col-xs-12 col-sm-12"><h3 style="color:white; font-size: 40px; font-family: 'HelveticaNeueLT-Bold'; padding-top: 30px" align="center">Come celebrate our new store</h3></div>
-  </div>
-  <div class="row" style="background:#f99d1c;">
-    <div class="col-xs-12 col-sm-12">
-      <h4 align="left" style="color:white; font-family: 'HelveticaNeueLT-Bold'; margin: 0"><span style="font-size: 20px;">Ribbon-Cutting</span> <span style="font-size: 16px">with the Honourable Maurizio Bevilacqua, Mayor of Vaughan</span></h4><br>
-      <h4 align="left" style="color:white; font-family: 'HelveticaNeueLT-Bold'; margin: 0"><span style="font-size: 20px;">Special donation Presentation</span> <span style="font-size: 16px">to support the Mackenzie Vaughan Hospital Foundation</span></h4>
-    </div>
-  </div>
-  <div class="row" style="background:#f99d1c;">
-    <div class="col-md-3" style="background: #00a64f; height: 200px; width: 200px; border-radius: 100px; text-align: center; margin-left: -30px; margin-top: 30px"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px; position: relative;top: 50%;transform: translateY(-50%);">Plus</h4></div>
-    <div class="col-md-9" style="margin-top: 25px; margin-bottom: 25px"">
+  <div class="row box orange offer">
+    <div class="col-md-2 circle green"><h4>Plus</h4></div>
+    <div class="col-md-9">
       <div class="row">
-        <div class="col-xs-3 col-md-3"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px;">FREE</h4></div>
-        <div class="col-xs-12 col-md-8"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">$25 Gift Card to <br>first 200 customers</h4></div>
+        <div class="col-xs-6 col-md-6"><h4>FREE</h4></div>
+        <div class="col-xs-6 col-md-6"><h4>FREE</h4></div>
       </div>
       <div class="row">
-        <div class="col-xs-3 col-md-3"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px;">FREE</h4></div>
-        <div class="col-xs-12 col-md-8"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">Reusable Shopping <br>Bag with $25 purchase</h4></div>
-      </div>
-      <div class="row">
-        <div class="col-xs-3 col-md-3"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 60px;">FREE</h4></div>
-        <div class="col-xs-12 col-md-8"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 24px;">food sampling and Complimentary <br>Coffee &amp; Cake in-store</h4></div>
+        <div class="col-xs-6 col-md-6"><h3>$25 Gift Card <br>to first 200 <br>customers</h3></div>
+        <div class="col-xs-6 col-md-6"><h3>Buy one reusable <br>shopping bag for $1, <br> get one free</h3></div>
       </div>
     </div>
   </div>
-  <div class="row" style="background:#f99d1c;">
-    <hr>
-    <div class="col-xs-12 col-sm-12"><h3 style="color:white; font-size: 40px; font-family: 'HelveticaNeueLT-Bold'; padding-top: 30px" align="center">Get your Grand Opening Coupon</h3></div>
+  <div class="row box orange date">
+    <div class="col-md-9">
+      <h3>Get these exclusive offers<br>
+      Friday, March 31 only at our<br>
+      fresh new Vaughan location!</h3>
+    </div>
+    <div class="col-md-3">
+      <a href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Highland+Farms+Grand+Opening&dates=20170331T100000Z/20170331T120000Z&details=For+details,+link+here:+http://www.hfstartfresh.ca&location=9940+Dufferin+Street,+Vaughan,+ON,+L6A+4K5&sf=true&output=xml#">
+        <input type="submit" aria-label="Submit" value="+ add to your calendar" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" >
+      </a>
+    </div>
   </div>
-  <div class="row" style="background:#f99d1c;">
-    <hr>
-    <div class="col-xs-12 col-md-8"><h1 style="color:white; font-size: 120px; font-family: 'HelveticaNeueLT-Bold';" align="center">SAVE $10</h1></div>
-    <div class="col-xs-6 col-md-4"><h1 style="color:white; font-size: 24px; font-family: 'HelveticaNeueLT-Bold';" align="left">when you<br>spend $50 or<br>more at our<br>Vaughan store</h1></div>
+  <div class="row store"> 
+    <div class="col-md-12">
+      <h1>You'll love our Vaughan store!</h1>
+    </div>
   </div>
-  <div class="row" style="background:#f99d1c;">
-    <div class="col-md-7"><p style="color:white; font-size: 12px; font-family: 'HelveticaNeueLT-Condensed'; line-height: 1" align="left">Valid March 31, 2017 only at our Vaughan location (9940 Dufferin St.) when you spend $150 or more before taxes. Coupon must be surrendered at time of purchase. Mobile technology redemption not available. Coupon limited to one per customer. Photocopies will not be accepted. No cash value. </p></div>
-    <div class="col-md-5"><input type="submit" aria-label="Submit" value="Sign up for your coupon >" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" style="background-color: #00a64f; margin:0 auto"></div>
-  </div>
-  <div class="row" style="background:white; margin-top: 30px"> 
-    <h1 style="color: #00a64f; font-size: 120px; font-family: 'HelveticaNeueLT-Bold'"">You'll love our Vaughan store!</h1>
-  </div>
-  <div class="row"> 
+  <div class="row store-image"> 
     <img src="<?=site_url()?>/assets/media/vaughan/store-front.jpg" style="width: 100%; height: auto; z-index:0.5">
-    <div class="col-md-3" style="background: #f99d1c; float: right; height: 200px; width: 200px; border-radius: 100px; text-align: center; margin-right: -30px; margin-top: -90%"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 36px; position: relative;top: 50%;transform: translateY(-50%); z-index:2">FRESH<br>&amp; NEW</h4></div>
+    <div class="col-md-3 orange circle"><h4>FRESH<br>&amp; NEW</h4></div>
     <div class="row">
-      <div class="col-md-4" style="margin-top: -120px">
-        <p style="color:white; font-size: 16px; font-family: 'SerifaBT-Light'; line-height: 1; padding-left: 20px">Redesigned from top to <br>bottom, with a bright layout, <br>colour-coded sections and <br>wide aisles, our new store <br>will make your shopping <br>experience more enjoyable!</p>
+      <div class="col-md-4 store-caption">
+        <span>Redesigned from top to <br>bottom, with a bright layout, <br>colour-coded sections and <br>wide aisles, our new store <br>will make your shopping <br>experience more enjoyable!</span>
       </div>
     </div>
   </div>
 </main>
 <div class="container-fluid">
   <div class="row">
-    <img src="<?=site_url()?>/assets/media/vaughan/deli-poultry.jpg" style="width: 100%; height: auto;">
+    <img src="<?=site_url()?>/assets/media/vaughan/deli-poultry.jpg" width="100%">
   </div>
 </div>
-<main style="margin-right: auto; margin-left: auto;">
-  <div class="row col-md-12 col-xs-12 col-lg-12">
-    <h1 style="color: #f99d1c; font-size: 100px; font-family: 'HelveticaNeueLT-Bold'"">Our fresh new look</h1>
+<main>
+  <div class="row deli">
+    <h1>Freshly sliced</h1>
   </div>
-  <div class="row col-md-12 col-xs-12 col-lg-12">
-    <p style="color:black; font-size: 20px; font-family: 'SerifaBT-Light';" >Diciis anduntent qui cum autet illenim que verio et voluptaspere et ex excerfe rferspidit eres sed quis et et ditas dolorum que volupta nimus as autemquo mintem quossit pernatectur</p>
+  <div class="row">
+    <p style="color:black; font-size: 20px; font-family: 'SerifaBT-Light';" >The legendary full service meat and deli counters offer a traditional market-style experience, with large selections of prime cuts, deli meats and cheeses.</p>
   </div>
-  <div class="row col-md-12 col-xs-12 col-lg-12">
+  <div class="row">
     <img src="<?=site_url()?>/assets/media/vaughan/produce.jpg" style="width: 100%; height: auto;">
+    <div class="col-md-3" style="background: #00a64f; float: right; height: 200px; width: 200px; border-radius: 100px; text-align: center; margin-right: -30px; margin-top: -95%"><h4 style="color: white; font-family: 'HelveticaNeueLT-Bold'; font-size: 36px; position: relative;top: 50%;transform: translateY(-50%); z-index:2">FARM <br>FRESH <br>EVERY DAY</h4></div>
+  </div>
+  <div class="row">
+    <h1 style="color: #00a64f; font-size: 100px; font-family: 'HelveticaNeueLT-Bold'">The freshest produce</h1>
+  </div>
+  <div class="row">
+    <p style="color:black; font-size: 20px; font-family: 'SerifaBT-Light';" >The size, selection and value of our produce is unsurpassed. Everything is hand-selected daily from local and international farmers, bringing you the freshest produce at the best prices.</p>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <img src="<?=site_url()?>/assets/media/vaughan/deli.jpg" style="width: 100%; height: auto;">
+    </div>
+    <div class="col-md-6">
+      <div class="row">
+      <h1 style="color: #00a64f; font-size: 100px; font-family: 'HelveticaNeueLT-Bold'">The freshest produce</h1>
+      </div>
+      <div class="row">
+        <p style="color:black; font-size: 20px; font-family: 'SerifaBT-Light';" >The size, selection and value of our produce is unsurpassed. Everything is hand-selected daily from local and international farmers, bringing you the freshest produce at the best prices.</p>
+    </div>
   </div>
 </main>
+<div class="container-fluid">
+  <div class="row">
+  <img src="<?=site_url()?>/assets/media/vaughan/country-kitchen.jpg" style="width: 100%; height: auto;">
+  </div>
+</div>
+<main>
+  <div class="row">
+    <h1 style="color: #00a64f; font-size: 100px; font-family: 'HelveticaNeueLT-Bold'">Freshly prepared</h1>
+  </div>
+  <div class="row">
+    <p style="color:black; font-size: 20px; font-family: 'SerifaBT-Light';" >Don’t worry if you’re running a little late. Our talented cooks whip up fresh and delicious meals such as chicken, ribs, pasta and a variety of sides – all ready to serve at a low price 
+and great taste. Let us take care of dinner tonight!</p>
+<hr>
+  </div>
+  <div class="row">
+    <h1 style="color: #f99d1c; font-size: 100px; font-family: 'HelveticaNeueLT-Bold'">Look for our deals... at your fingertips! </h1>
+  </div>
+  <div class="row">
+    <p style="color:black; font-size: 20px; font-family: 'SerifaBT-Light';" >Great deals and savings every week! Subscribe to our e-flyer.</p>
+  </div>
+  <div class="row">
+  <div class="col-xs-8 col-sm-8 col-md-offset-2">
+        <!-- Begin MailChimp Signup Form -->
+            <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                <input type="email" value="" name="EMAIL" style="width:97%" id="mce-EMAIL" aria-label="email" placeholder="email address" aria-required="true" required>
+                <div style="position: absolute; left: -5000px;"><input type="text" name="b_a574aa827269d018202389912_792b79af52" tabindex="-1" value="" placeholder="Your e-mail address"></div>
+                <input type="submit" aria-label="Subscribe" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email sticky right green" style="background-color:#f99d1c">
+            </form>
+        </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 social-icons" align="center">
+          <a href="http://www.facebook.com/sharer.php?s=100&p[url]=http://hfstartfresh.ca" alt="facebook" class="facebook-landing"></a>
+          <a href="http://twitter.com/intent/tweet?text=A+fresh+new+Highland+Farms+is+coming+to+Vaughan&hashtags=hfstartfresh" alt="twitter" class="twitter-landing"></a>
+          <a href="https://www.instagram.com/highlandfarms_on/" alt="instagram" class="instagram-landing"></a><br>
+        </div>
+  </div>
+</main>
+
+<script type="text/javascript" async src="<?=site_url()?>/assets/js/vaughan-countdown.js"></script>
+<script type="text/javascript" async src="https://platform.twitter.com/widgets.js"></script>
