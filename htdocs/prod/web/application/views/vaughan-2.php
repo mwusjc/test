b959cac34629a2334923e82fcb5fc79a2a3074d6
Michael Wu <michael.wu@P14008-MWu-10-3-100-167.vlan-3.stjosephcontent.com>
2017-03-22 10:29:32 -0400

    Re #455 - updated main landing page

--- htdocs/prod/web/application/views/vaughan-2.php
+++ htdocs/prod/web/application/views/vaughan-2.php
@@ -27,7 +27,7 @@
   <div class="row box orange rounded intro">
     <div class="col-xs-12 col-sm-12">
     <h1>Join us Friday,<br>
-    March 31 at 10am for<br>
+    March 31 at 10am for<br class="breakpoint">
     our Grand Opening!</h1><br>
     <span style="font-family: 'SerifaBT-Light';" align="center">9940 Dufferin St. at Major MacKenzie Dr. W.</span>
     </div>
@@ -41,7 +41,7 @@
       </div>
       <div class="row details">
         <div class="col-xs-6 col-md-6"><h3>$25 Gift Card <br>to first 200 <br>customers</h3></div>
-        <div class="col-xs-6 col-md-6"><h3>Eco-friendly <br>shopping bag with <br>every purchase</h3></div>
+        <div class="col-xs-6 col-md-6"><h3>Eco-friendly <br>shopping bag with <br class="breakpoint">every purchase</h3></div>
       </div>
     </div>
   </div>
@@ -52,7 +52,7 @@
       fresh new Vaughan location!</h3>
     </div>
     <div class="col-md-4">
-      <a href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Highland+Farms+Grand+Opening&dates=20170331T100000Z/20170331T120000Z&details=For+details,+link+here:+http://www.hfstartfresh.ca&location=9940+Dufferin+Street,+Vaughan,+ON,+L6A+4K5&sf=true&output=xml#">
+      <a href="<?=site_url()?>/assets/media/vaughan/Highland_Farms_Vaughan_Grand_Opening.ics">
         <input type="submit" aria-label="Submit" value="+ add to your calendar" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" >
       </a>
     </div>
@@ -99,7 +99,7 @@
     </div>
     <div class="col-md-6">
       <div class="row">
-      <h1>Live and eat well</h1>
+        <h1>Live and eat well</h1>
       </div>
       <div class="row">
         <p>We know how important health and wellness is to our customers. That’s why our new store features a huge selection of gluten-free, natural and organic products.</p>
@@ -118,39 +118,30 @@
 and great taste. Let us take care of dinner tonight!</p>
   </div>
   <div class="row aisle">
-  <img src="<?=site_url()?>/assets/media/vaughan/aisle.jpg" style="width: 100%; height: auto;"><br>
-  <h1>Great prices in every aisle</h1>    
-  <p>Get competitive prices on thousands of brand names throughout the store.</p>
+    <img src="<?=site_url()?>/assets/media/vaughan/aisle.jpg" style="width: 100%; height: auto;"><br>
+    <h1>Great prices in every aisle</h1>    
+    <p>Get competitive prices on thousands of brand names throughout the store.</p>
     <hr>
   </div>
   <div class="row flyer-deals">
     <h1>Look for our deals... at your fingertips!</h1>
     <p>Great deals and savings every week! Subscribe to our e-flyer.</p>
-  <div class="col-xs-8 col-sm-8 col-md-offset-2 vaughan-subscribe-2 desktop">
-        <!-- Begin MailChimp Signup Form -->
-            <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-                <input type="email" value="" name="EMAIL" style="width:97%" id="mce-EMAIL" aria-label="email" placeholder="email address" aria-required="true" required>
-                <div style="position: absolute; left: -5000px;"><input type="text" name="b_a574aa827269d018202389912_792b79af52" tabindex="-1" value="" placeholder="Your e-mail address"></div>
-                <input type="submit" aria-label="Subscribe" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email sticky right green" style="background-color:#f99d1c">
-            </form>
-        </div>
+    <div class="col-xs-8 col-sm-8 col-md-offset-2 vaughan-subscribe-2 desktop">
+    <!-- Begin MailChimp Signup Form -->
+      <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+        <input type="email" value="" name="EMAIL" style="width:97%" id="mce-EMAIL" aria-label="email" placeholder="email address" aria-required="true" required>
+         <div style="position: absolute; left: -5000px;"><input type="text" name="b_a574aa827269d018202389912_792b79af52" tabindex="-1" value="" placeholder="Your e-mail address"></div>
+        <input type="submit" aria-label="Subscribe" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email sticky right green" style="background-color:#f99d1c">
+      </form>
+    </div>
     <div class="vaughan-subscribe-2 mobile">
-        <!-- Begin MailChimp Signup Form -->
-            <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-                <input type="email" value="" name="EMAIL" style="width:100%" id="mce-EMAIL" aria-label="email" placeholder="email address" aria-required="true" required>
-                <div style="position: absolute; left: -5000px;"><input type="text" name="b_a574aa827269d018202389912_792b79af52" tabindex="-1" value="" placeholder="Your e-mail address"></div>
-                <input type="submit" aria-label="Subscribe" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" style="background-color:#f99d1c">
-            </form>
-        </div>
-  </div>
-
-  </div>
-  <div class="row">
-    <div class="col-xs-12 col-sm-12 social-icons" align="center">
-          <a href="http://www.facebook.com/sharer.php?s=100&p[url]=http://hfstartfresh.ca" alt="facebook" class="facebook-landing"></a>
-          <a href="http://twitter.com/intent/tweet?text=A+fresh+new+Highland+Farms+is+coming+to+Vaughan&hashtags=hfstartfresh" alt="twitter" class="twitter-landing"></a>
-          <a href="https://www.instagram.com/highlandfarms_on/" alt="instagram" class="instagram-landing"></a><br>
-        </div>
+      <!-- Begin MailChimp Signup Form -->
+      <form action="//highlandfarms.us9.list-manage.com/subscribe/post?u=a574aa827269d018202389912&amp;id=792b79af52" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+        <input type="email" value="" name="EMAIL" style="width:100%" id="mce-EMAIL" aria-label="email" placeholder="email address" aria-required="true" required>
+        <div style="position: absolute; left: -5000px;"><input type="text" name="b_a574aa827269d018202389912_792b79af52" tabindex="-1" value="" placeholder="Your e-mail address"></div>
+        <input type="submit" aria-label="Subscribe" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="subscribe-email" style="background-color:#f99d1c">
+      </form>
+    </div>
   </div>
 </main>
 
