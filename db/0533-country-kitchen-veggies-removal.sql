2abc3f4138f3a2e2b0b3b9fab0f28ffa38806932
Michael Wu <michael.wu@P14008-MWu.local>
2017-05-01 09:33:56 -0400

    Re #533 - removed grilled veggies from ck db, locally

---
+++ db/0533-country-kitchen-veggies-removal.sql
@@ -0,0 +1,5 @@
+-- --------------------------------------------------------
+-- https://sjc-istudio.assembla.com/spaces/www-highlandfarms-ca/tickets/realtime_cardwall?ticket=533
+-- --------------------------------------------------------
+USE `webdev_hlf`;
+DELETE FROM products WHERE products.id = 22;
