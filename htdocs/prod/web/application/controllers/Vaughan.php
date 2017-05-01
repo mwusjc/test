e945007fe8f9c7757c2f77151c0aec55aa8d35b9
Michael Wu <michael.wu@P14008-MWu.local>
2017-05-01 10:14:31 -0400

    Re #534 - committing from develop, implemented php 301 for vaughan page instead of htaccess 301

---
+++ htdocs/prod/web/application/controllers/Vaughan.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Vaughan extends CI_Controller {
+
+    public function index()
+    {
+
+        $this->load->view("header", array('title' => "A Fresh Look At All Our Store Departments | Highland Farms", "desc" => "Every shopping trip is a flavour adventure, with shelves and counters fully stocked with favourites from around the world. Our deli & meat counter is legendary!"));
+        $this->load->view("vaughan");
+        $this->load->view("footer");
+    }
+}
